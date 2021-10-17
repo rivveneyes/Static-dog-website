e0015a9211fe55b356db8e1d758b763232d3f811
Joshua Garcia <garciajoshua907@yahoo.com>
2021-10-17 10:02:01 -0600

    inital web page start

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const navigation = document.getElementById("navigation");
+const divOne = document.getElementById("div2");
+const displayDogs = document.getElementsByClassName("dogsDisplay");
+
+async function getDogs() {
+  dogImages = await fetch("https://dog.ceo/api/breeds/image/random/3").then(
+    (res) => res.json()
+  );
+  dogImages.message.forEach((element, idx) => {
+    displayDogs[idx].src = element;
+  });
+}
+
+getDogs();
+
+window.onscroll = () => {
+  Sticknav();
+};
+
+// ======functions
+function Sticknav() {
+  if (navigation.offsetHeight < window.pageYOffset) {
+    navigation.classList.add("stickNav");
+  } else {
+    navigation.classList.remove("stickNav");
+  }
+}

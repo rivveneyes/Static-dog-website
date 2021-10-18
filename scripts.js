cb4cb1fd49e818ac8c47466906468f6d63f1cee8
Joshua Garcia <garciajoshua907@yahoo.com>
2021-10-17 20:49:42 -0600

    added img set up with first desc

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
-const navigation = document.getElementById("navigation");
-const divOne = document.getElementById("div2");
-const displayDogs = document.getElementsByClassName("dogsDisplay");
+// const navigation = document.getElementById("navigation");
+// const divOne = document.getElementById("div2");
+// const displayDogs = document.getElementsByClassName("dogsDisplay");
 
-async function getDogs() {
-  dogImages = await fetch("https://dog.ceo/api/breeds/image/random/3").then(
-    (res) => res.json()
-  );
-  dogImages.message.forEach((element, idx) => {
-    displayDogs[idx].src = element;
-  });
-}
+// async function getDogs() {
+//   dogImages = await fetch("https://dog.ceo/api/breeds/image/random/3").then(
+//     (res) => res.json()
+//   );
+//   dogImages.message.forEach((element, idx) => {
+//     displayDogs[idx].src = element;
+//   });
+// }
 
-getDogs();
+// getDogs();
 
-window.onscroll = () => {
-  Sticknav();
-};
+// window.onscroll = () => {
+//   Sticknav();
+// };
 
 // ======functions
 function Sticknav() {
